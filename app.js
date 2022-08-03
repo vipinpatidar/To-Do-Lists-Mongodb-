2378fd3c5956d89b7367e446ea73efddfd02f7ea
Vipin patidar <vipinpatidar5@gmail.com>
2022-08-03 18:49:03 +0530

    changed in public path

--- app.js
+++ app.js
@@ -11,7 +11,8 @@ const app = express();
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static("public"));
+// app.use(express.static("public"));
+app.use(express.static(path.join(__dirname,'public')))
 
 /* -----------------------------------------------------------------------------------------------------------------------------------------*/
 // Adding mongodb and mongoose
