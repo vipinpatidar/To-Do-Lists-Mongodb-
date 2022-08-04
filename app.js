31d3a3ab8c23421719194ade20481653a1d17043
Vipin patidar <vipinpatidar5@gmail.com>
2022-08-04 10:15:07 +0530

    remove path

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
 // app.use(express.static("public"));
-app.use(express.static(path.join(__dirname,'public')))
+app.use(express.static(__dirname,'public'))
 
 /* -----------------------------------------------------------------------------------------------------------------------------------------*/
 // Adding mongodb and mongoose
