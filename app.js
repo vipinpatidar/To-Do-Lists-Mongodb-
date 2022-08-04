03d9d76d90856a842e0f779a43f61a0670659ee2
Vipin patidar <vipinpatidar5@gmail.com>
2022-08-04 10:45:59 +0530

    remove __dirname

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
 // app.use(express.static("public"));
-app.use(express.static(__dirname,'public'))
+app.use(express.static('public'))
 
 /* -----------------------------------------------------------------------------------------------------------------------------------------*/
 // Adding mongodb and mongoose
